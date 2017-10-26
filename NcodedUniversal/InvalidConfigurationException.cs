5022b1e44abb020f275c54cd71a9795d943f83e7
Jens Marchewka <im.naice@googlemail.com>
2017-10-26 16:55:52 +0200

    Added NcodedUniversal to project

---
+++ NcodedUniversal/InvalidConfigurationException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NcodedUniversal
+{
+    public class InvalidConfigurationException : Exception
+    {
+        public InvalidConfigurationException(string defaultConfig)
+            : base($"Invalid configuration detected. No {defaultConfig} set. Setup default dependecys via {nameof(Configuration)} class before using.")
+        {
+
+        }
+    }
+}
